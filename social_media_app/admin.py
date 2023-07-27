b6e585e36181a3c8ccabdec538739c1e30e850da
vamsigitlab <vamsiparupalli1@gmail.com>
2023-07-27 22:52:42 +0530

    Added the social media app

---
+++ social_media_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from social_media_app.models import Profile, Post, LikePost
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(LikePost)
+
